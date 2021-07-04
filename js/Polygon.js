e34b497c5f470457bc078efb137109fbbdf14fc9
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-07-04 18:09:01 +0530

    Add files via upload

--- js/Polygon.js
+++ js/Polygon.js
@@ -18,7 +18,6 @@ World.add(world, this.body);
 display(){
     var pos =this.body.position;
     push()
-    translate(pos.x,pos.y);
     imageMode(CENTER);
     image(this.image,0,0, this.width, this.height);
     pop()
