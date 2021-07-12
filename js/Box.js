3f5115a139bc2b0972f5dff9dcef643a4ff48a07
prathik402 <73998548+prathik402@users.noreply.github.com>
2021-07-12 16:13:43 +0530

    Add files via upload

--- js/Box.js
+++ js/Box.js
@@ -2,33 +2,43 @@ class Box {
  constructor(x,y,width,height){
  var options = {
  isStatic: false,
- density: 1,
- friction: 1,
+ restitution: 0.4,
+ friction: 0.0,
  
   }
- 
+  this.visibility = 255;
  this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);   
-      x= this.x
-      y= this.y
-      this.Visiblity = 255;
+     
+      
 
       
  }   
+ score(){
+   if(this.visibility<0 && this.visibility >-105){
+     score++;
+   }
+ }
  display(){
-    var pos =this.body.position;
-    fill("red");
-    square(pos.x, pos.y, this.width, this.height);
-    
-    if(this.body.speed < 1){
-      World.remove(world, this.body);
+    console.log(this.body.speed);
+    if(this.body.speed <3){
+      var angle = this.body.angle;
+      var pos = this.body.position
       push();
-      this.Visiblity = this.Visiblity - 5;
-     tint(255,this.Visiblity);
-     pop();
-     }
-     
+      translate(pos.x,pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      rect(0,0,this.width,this.height);
+      pop();
+    }else{
+      World.remove(world,this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      pop();
+   }
+  
   }
+
 }
